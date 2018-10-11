d98ca26e2684f0035a001435f2e8a2c3b46ab08f
Natalie <thefuzzyslug@gmail.com>
2018-10-10 19:59:31 -0700

    fixed text alignments

--- js/effects.js
+++ js/effects.js
@@ -1,2 +1,15 @@
 AOS.init();
 $(window).load(function() { setTimeout(function() { AOS.refresh(); }, 500); });
+
+//preload hidden images for faster mouseover effects
+function preload(images) {
+    if (document.images) {
+        var i = 0;
+        var imageArray = new Array();
+        imageArray = images.split(',');
+        var imageObj = new Image();
+        for(i=0; i<=imageArray.length-1; i++) {
+            imageObj.src=images[i];
+        }
+    }
+}
